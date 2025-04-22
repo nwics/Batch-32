485e80739bb60d674bd79584ba4ba999a40469b5
nwics <nabilwicaksono274@gmail.com>
2025-04-22 13:33:44 +0700

    java day1

---
+++ Java/src/challenge1Day01.java
@@ -0,0 +1,157 @@
+public class challenge1Day01 {
+
+    public static void main(String[] args) {
+        // System.out.println("hello world");
+        int num = 5;
+
+        System.out.println();
+        System.out.print("soal no 1 " + "-".repeat(100));
+        System.out.println();
+        // soal no 1
+        int angka = 1;
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                System.out.print(angka++ + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.print("soal no 2 " + "-".repeat(100));
+        System.out.println();
+
+        // soal no 2
+        for (int i = 0; i < num; i++) {
+            int angkaa = i + 1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(angkaa++ + "\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.print("soal no 3 " + "-".repeat(100));
+        System.out.println();
+
+        // soal no 3
+        for (int i = 0; i < num; i++) {
+            // int angkaa = i + 1;
+            for (int j = 0; j < num - i; j++) {
+                // int num = j + 1;
+                System.out.print(j + i + 1 + "\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.print("soal no 4 " + "-".repeat(100));
+        System.out.println();
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                if (i <= j) {
+                    System.out.print(j + 1 + "\t");
+                } else {
+                    System.out.print(" \t");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.print("soal no 5 " + "-".repeat(100));
+        System.out.println();
+        // soal no 5
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                if (i == j) {
+                    System.out.print((i + 1) + "\t");
+                } else if (i < j) {
+
+                    System.out.print("10\t");
+                } else {
+                    System.out.print("20\t");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.print("soal no 6 " + "-".repeat(100));
+        System.out.println();
+        // soal no 6
+        for (int i = num; i > 0; i--) {
+            for (int j = num; j > 0; j--) {
+                if (i == j) {
+                    System.out.print((i) + "\t");
+                } else if (i > j) {
+                    System.out.print("20\t");
+                } else {
+                    System.out.print("10\t");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.print("soal no 7 " + "-".repeat(100));
+        System.out.println();
+
+        // soal no 7
+        int n = 8;
+        for (int i = 0; i < n; i++) {
+            int numm = n - i;
+            for (int j = numm; j >= 1; j--) {
+                System.out.print(j + "\t");
+            }
+            for (int j = 2; j <= numm; j++) {
+                System.out.print(j + "\t");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.print("soal no 9 " + "-".repeat(100));
+        System.out.println();
+
+        // soal no 9
+        for (int i = 0; i < num; i++) {
+            int angka9 = num;
+            for (int j = 0; j < num; j++) {
+                if (i % 2 == 0) {
+                    System.out.print(angka9-- + "\t");
+                } else {
+                    System.out.print((j + 1) + "\t");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.print("soal no 10 " + "-".repeat(100));
+        System.out.println();
+
+        // soal no 10
+
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                if (i % 2 == 0) {
+                    if (j % 2 != 0) {
+                        System.out.print(j + 1 + "\t");
+                    } else {
+                        System.out.print("-" + "\t");
+                    }
+                } else {
+                    if (j % 2 == 0) {
+                        System.out.print((j + 1) + "\t");
+                    } else {
+                        System.out.print("-" + "\t");
+                    }
+                }
+
+            }
+            System.out.println();
+        }
+
+    }
+
+}
---
+++ Java/src/challenge2Day01.java
@@ -0,0 +1,111 @@
+import java.util.HashMap;
+
+public class challenge2Day01 {
+
+    public static void main(String[] args) {
+        // System.out.println("hello world");
+        System.out.println("Reverse : " + Reverse(12));
+        System.out.println("isPalindrome : " + isPalindrome(121));
+        System.out.println("Capitalize : " + Capitalize("belajar java"));
+        System.out.println("IsNoDuplicateChar : " + IsNoDuplicateChar("abccc"));
+        System.out.println("IsBrace : " + IsBrace("(())"));
+    }
+
+    /*
+     * 1. Reverse
+     * input : 42
+     * output : 24
+     * 
+     */
+
+    public static int Reverse(int n) {
+        int result = 0;
+        while (n > 0) {
+            int num = n % 10;
+            result = result * 10 + num;
+            n = n / 10;
+        }
+        return result;
+    }
+
+    /*
+     * 2.isPalindrome
+     * input : 121
+     * output : true
+     */
+    public static boolean isPalindrome(int n) {
+        int result = 0;
+        int number = n;
+        while (n > 0) {
+            int num = n % 10;
+            result = result * 10 + num;
+            n = n / 10;
+        }
+        return result == number;
+    }
+
+    /*
+     * 3. Capitalize
+     * 
+     * input : "this is a very special title"
+     * output : "This Is A Very Special Title"
+     * 
+     */
+    public static String Capitalize(String input) {
+        String[] s = input.split(" ");
+        String result = "";
+        for (int i = 0; i < s.length; i++) {
+            String capital = s[i].substring(0, 1).toUpperCase() + s[i].substring(1);
+            result += capital;
+
+            if (i < s.length - 1) {
+                result += " ";
+            }
+        }
+        return result;
+    }
+
+    /*
+     * 4.IsNoDuplicateChar
+     * input : "abcde"
+     * output : true
+     */
+
+    public static boolean IsNoDuplicateChar(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /*
+     * 5.IsBrace
+     * input :"(())"
+     * output : true
+     * 
+     * input :"((()"
+     * output : false
+     */
+
+    public static boolean IsBrace(String s) {
+        int n = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                n++;
+            } else if (s.charAt(i) == ')') {
+                n--;
+            }
+            if (n < 0) {
+                return false;
+            }
+
+        }
+
+        return n == 0;
+    }
+}
